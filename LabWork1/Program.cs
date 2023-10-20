6a0fea7726cb305d89a3975b2ea6c7835f2ed554
Andrey Sharipov <andrey.sharipov1@gmail.com>
2023-10-20 19:45:24 +0300

    Добавьте файлы проекта.

---
+++ LabWork1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            Console.WriteLine("Cube the product of two numbers\n" +
+                "Enter the first number: ");
+
+            firstNumber = inputNumber();
+
+            Console.WriteLine("Enter the second number: ");
+            secondNumber = inputNumber();
+
+            double result = Math.Pow(firstNumber * secondNumber, 3);
+
+            Console.WriteLine("Your result: " + result);
+
+
+
+            int questionNumbers;
+
+            for (int i = 1; i <= 5; i++) {
+                questionNumbers = (i * 21) % 24 + 1;
+                Console.WriteLine("Question number: " + questionNumbers);
+            }
+        }
+
+        public static int inputNumber()
+        {
+            String input = "";
+            int number = 0;
+            while (true)
+            {
+                try
+                {
+                    input = Console.ReadLine();
+                    number = int.Parse(input);
+                    return number;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wrong input");
+                }
+            }
+        }
+    }
+}
